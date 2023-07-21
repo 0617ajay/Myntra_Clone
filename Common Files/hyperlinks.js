7a4f1d2126a5eabb0cd1cbf604e3c0907cc43e92
Ajay <136958093+0617ajay@users.noreply.github.com>
2023-07-22 01:54:15 +0530

    Add files via upload

---
+++ Common Files/hyperlinks.js
@@ -0,0 +1,10 @@
+
+
+// HYPERLINKS
+document.querySelector('.fa-heart').addEventListener('click', function(){
+    window.location.href = "../wishlist/wishlist.html"
+})
+document.querySelector(".fa_wishlist").addEventListener('click', function(){
+    window.location.href = "../wishlist/wishlist.html"
+})
+
